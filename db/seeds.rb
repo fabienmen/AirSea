dac5c04f7cfec40ed84356a89b3838166e5c2eed
Joseph Flynn <joseph.f.flynn@gmail.com>
2023-05-31 14:33:42 +0100

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Boat.destroy_all
+
+
+
+puts "Creating Boats..."
+10.times do ||
+boat10 = Boat.Create(
+  name: "Tranquility", 
+  price_daily: 80, 
+  size: 40.5, 
+  descrpition: "A beautiful sail boat that will carry your dreams with the sunset on the horizon.",
+  production_year: 2005,
+  # boat_picture: "/"
+)
+puts "Created #{boat10.name}"
