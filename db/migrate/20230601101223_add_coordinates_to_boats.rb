0bc00fb545a43640886b0ae44f1dec26e73170cd
fabienmen <fabien.mendez@gmail.com>
2023-06-01 15:37:14 +0100

    Revert "added complete map on boat index"

--- db/migrate/20230601101223_add_coordinates_to_boats.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToBoats < ActiveRecord::Migration[7.0]
-  def change
-    add_column :boats, :latitude, :float
-    add_column :boats, :longitude, :float
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,7 @@ boat10 = Boat.create(
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon.",
   production_year: 2005,
   category: "Yacht",
-  user: user1,
-  latitude: 14.601982,
-  longitude: -61.064570
+  user: user1
   # boat_picture: "/"
 )
 Boat.last.photo.attach(io: File.open('app/assets/images/Yacht.jpeg'), filename:'Yacht.jpeg')
@@ -34,8 +32,6 @@ boat9 = Boat.create(
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon.",
   production_year: 2010,
   category: "Catamaran",
-  latitude: 38.913588,
-  longitude: 1.442004,
   user: user1
   # boat_picture: "/"
 )
@@ -49,8 +45,6 @@ boat8 = Boat.create(
   description: "A beautiful fishing boat that will carry your dreams with the sunset on the horizon.",
   production_year: 2016,
   category: "Fishing Boat",
-  latitude: 38.701689,
-  longitude: -9.168002,
   user: user1
   # boat_picture: "/"
 )
@@ -58,14 +52,12 @@ Boat.last.photo.attach(io: File.open('app/assets/images/Fishing Boat.jpeg'), fil
 puts "Created #{boat8.name}"
 
 boat7 = Boat.create(
-  name: "Le Papy",
+  name: "Boaty Boat",
   price_daily: 70,
   size: 20,
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon.",
   production_year: 1995,
   category: "Sailing Boat",
-  latitude: 43.077068,
-  longitude: 5.803693,
   user: user1
   # boat_picture: "/"
 )
@@ -79,8 +71,6 @@ boat6 = Boat.create(
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon.",
   production_year: 2008,
   category: "Speedboat",
-  latitude: 37.063391,
-  longitude: 15.288334,
   user: user1
   # boat_picture: "/"
 )
@@ -94,8 +84,6 @@ boat5 = Boat.create(
   description: "A beautiful Jetski that will carry your dreams with the sunset on the horizon.",
   production_year: 1970,
   category: "Jetski",
-  latitude: 41.388695,
-  longitude: 9.163490,
   user: user1
   # boat_picture: "/"
 )
@@ -109,8 +97,6 @@ boat4 = Boat.create(
   description: "A beautiful fishing boat that will carry your dreams with the sunset on the horizon.",
   production_year: 2003,
   category: "Fishing Boat",
-  latitude: 37.935442,
-  longitude: 23.687222,
   user: user1
   # boat_picture: "/"
 )
@@ -124,8 +110,6 @@ boat3 = Boat.create(
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon.",
   production_year: 2005,
   category: "Yacht",
-  latitude: -23.802957,
-  longitude: -45.397850,
   user: user1
   # boat_picture: "/"
 )
@@ -139,8 +123,6 @@ boat2 = Boat.create(
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon. Enjoy riding with Dolphins for your trip!",
   production_year: 2006,
   category: "Catamaran",
-  latitude: -36.815395,
-  longitude: -73.086526,
   user: user1
   # boat_picture: "/"
 )
@@ -154,8 +136,6 @@ boat1 = Boat.create(
   description: "A beautiful sail boat that will carry your dreams with the sunset on the horizon. Watch whales swimming around and enjoy the majesty",
   production_year: 1995,
   category: "Speedboat",
-  latitude: 33.742760,
-  longitude: -118.232665,
   user: user1
   # boat_picture: "/"
 )
